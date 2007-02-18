8575cf6950ceea2eac11e30d650bc2537268b20a
Andreas Zeidler <witsch@plone.org>
2007-02-18 21:03:30 +0000

    set up package structure for plone.app.customerize
    svn path=/plone.app.customerize/trunk/; revision=12637

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(name = 'plone.app.customerize',
+      version = '1.0a3',
+      description = 'Integrate five.customerize into Plone.',
+      keywords = 'customerize plone five views page templates zmi',
+      author = 'Plone Foundation',
+      author_email = 'plone-developers@lists.sourceforge.net',
+      url = 'http://svn.plone.org/svn/plone/plone.app.customerize/',
+      download_url = 'http://cheeseshop.python.org/pypi/plone.app.customerize/',
+      license = 'GPL',
+      packages = find_packages(),
+      namespace_packages = ['plone.app'],
+      include_package_data = True,
+      install_requires = ['setuptools',],
+      platforms = 'Any',
+      zip_safe = False,
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Zope2',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      long_description = """\
+        This packages integrates five.customerize_ into Plone, which enables
+        site administrators to customize five/zope3-style views TTW via the
+        ZMI in a way similar to it's possible to customize filesystem-based
+        skin elements via the portal skin "customize" procedure.
+        
+          .. _five.customerize: http://svn.zope.org/five.customerize/ """,
+)
+
