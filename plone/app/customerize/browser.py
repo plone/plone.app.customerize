0f88444139903e0bb0f9f320351c16042562c4fb
Andreas Zeidler <witsch@plone.org>
2007-02-19 21:55:58 +0000

    added overview of all registered views as the default view for the `portal_view_customizations` tool along with zmi views for customization of the listed views
    in consequence a fair chunk of code from five.customerize's [http://svn.zope.org/five.customerize/trunk/src/five/customerize/browser.py browser.py] was needed to be adapted to the fact that the customization is not started at a particular object (as with `zptviews.html`), but instead all interfaces and their registered views are listed;  in other words, we don't have access to an instance of a class/interface the view in question was registered for, so we cannot determine various information by using `getMultiAdapter` or, more generally speaking, the context, either
    
    on the other hand, this approach allows us to see all view registrations at once instead of "just" those relevant to the context, which also includes views registered for objects that are not even used in the site (yet);  also, this way the context is not needed anymore in a lot of function, so they can be factored out into a small helper library, which might help cleaning up a little...
    
    svn path=/plone.app.customerize/trunk/; revision=12742

--- plone/app/customerize/browser.py
+++ plone/app/customerize/browser.py
@@ -1,7 +1,10 @@
 from Products.Five.browser import BrowserView
+from five.customerize.interfaces import IViewTemplateContainer
 from five.customerize.browser import mangleAbsoluteFilename
+from five.customerize.zpt import TTWViewTemplate
 from zope.publisher.interfaces.browser import IBrowserRequest
-from zope.component import getGlobalSiteManager
+from zope.component import getGlobalSiteManager, getSiteManager, getUtility
+from os.path import sep
 
 
 def getViews(type):
@@ -14,6 +17,11 @@ def getViews(type):
             for required_iface in reg.required[:-1]:
                 yield reg
 
+def interfaceName(iface):
+    """ return a sensible name for the given interface """
+    name = getattr(iface, '__name__', repr(iface))
+    return getattr(iface, '__identifier__', name)
+
 
 class RegistrationsView(BrowserView):
 
@@ -22,8 +30,8 @@ class RegistrationsView(BrowserView):
             factory = reg.factory
             while hasattr(factory, 'factory'):
                 factory = factory.factory
-            #XXX this should really be dealt with using a marker interface
-            # on the view factory
+            # TODO: this should really be dealt with using
+            # a marker interface on the view factory
             if hasattr(factory, '__name__') and \
                    factory.__name__.startswith('SimpleViewClass'):
                 yield reg
@@ -34,8 +42,85 @@ class RegistrationsView(BrowserView):
         for reg in sorted(self.templateViewRegistrations(), key=regkey):
             yield {
                 'viewname': reg.name,
-                'for': getattr(reg.required[0], '__identifier__', '???'),
-                'type': getattr(reg.required[1], '__identifier__', '???'),
+                'for': interfaceName(reg.required[0]),
+                'type': interfaceName(reg.required[1]),
                 'zptfile': mangleAbsoluteFilename(reg.factory.index.filename),
                 'zcmlfile': mangleAbsoluteFilename(reg.info.file)
             }
+
+    def templateViewRegistrationGroups(self):
+        ifaces = {}
+        for reg in self.templateViewRegistrationInfos():
+            key = reg['for']
+            if ifaces.has_key(key):
+                ifaces[key]['views'].append(reg)
+            else:
+                ifaces[key] = { 'name': key, 'views': [reg] }
+        return sorted(ifaces.values(), cmp=lambda a,b: cmp(a['name'], b['name']))
+
+    def generateIdFromRegistration(self, reg):
+        return '%s-%s' % (
+            interfaceName(reg.required[0]).lower(),
+            reg.name
+        )
+
+    def getViewClassFromRegistration(self, reg):
+        # The view class is generally auto-generated, we usually want
+        # the first base class, though if the view only has one base
+        # (generally object or BrowserView) we return the full class
+        # and hope that it can be pickled        
+        klass = reg.factory
+        base = klass.__bases__[0]
+        if base is BrowserView or base is object:
+            return klass
+        return base
+
+    def getTemplateCodeFromRegistration(self, reg):
+        template = reg.factory.index
+        # TODO: we can't do template.read() here because of a bug in
+        # Zope 3's ZPT implementation.
+        return open(template.filename, 'rb').read()
+
+    def getViewPermissionFromRegistration(self, reg):
+        permissions = reg.factory.__ac_permissions__
+        for permission, methods in permissions:
+            if methods[0] in ('', '__call__'):
+                return permission
+
+    def createTTWViewTemplate(self, reg):
+        return TTWViewTemplate(
+            id = str(self.generateIdFromRegistration(reg)),
+            text = self.getTemplateCodeFromRegistration(reg),
+            view = self.getViewClassFromRegistration(reg),
+            permission = self.getViewPermissionFromRegistration(reg))
+
+    def registerTTWView(self, viewzpt, reg):
+        sm = getSiteManager(self.context)
+        sm.registerAdapter(viewzpt, required = reg.required,
+                           provided = reg.provided, name = reg.name)
+
+    def doCustomizeTemplate(self, reg):
+        viewzpt = self.createTTWViewTemplate(reg)
+        container = getUtility(IViewTemplateContainer)
+        viewzpt = container.addTemplate(viewzpt.getId(), viewzpt)
+        self.registerTTWView(viewzpt, reg)
+        return viewzpt
+
+    def findTemplateViewRegistration(self, for_name, type_name, viewname):
+        for reg in self.templateViewRegistrations():
+            if interfaceName(reg.required[0]) == for_name and \
+               interfaceName(reg.required[1]) == type_name and \
+               reg.name == viewname:
+                return reg
+
+    def getRegistrationFromRequest(self):
+        form = self.context.request.form
+        return self.findTemplateViewRegistration(form['for_name'],
+            form['type_name'], form['view_name'])
+        
+    def customizeTemplate(self):
+        req = self.getRegistrationFromRequest()
+        viewzpt = self.doCustomizeTemplate(req)
+        url = sep.join(viewzpt.getPhysicalPath()) + "/manage_workspace"
+        self.request.response.redirect(url)
+
