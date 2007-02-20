35b4fa2312e47ddd2501d74efb4f58e6f3b76cc0
Andreas Zeidler <witsch@plone.org>
2007-02-20 11:12:56 +0000

    added an icon (which still requires the module to be registered as a zope2 product... :()
    svn path=/plone.app.customerize/trunk/; revision=12801

--- plone/app/customerize/__init__.py
+++ plone/app/customerize/__init__.py
@@ -1,1 +1,12 @@
-# make this directory a package
+from Products.CMFCore.utils import ToolInit
+from plone.app.customerize.tool import ViewTemplateContainer
+
+
+def initialize(context):
+    """ initialize function called when used as a zope2 product """
+
+    ToolInit('plone.app.customerize',
+             tools = (ViewTemplateContainer,),
+             icon = 'tool.gif',
+    ).initialize(context)
+
