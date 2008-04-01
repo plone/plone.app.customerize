d504ddef5b4cb26d513e184b96843296679ab9dd
Stefan H. Holek <stefan@epy.co.at>
2008-04-01 11:19:45 +0000

    Derived layers *must* have setUp and tearDown methods, even when they are empty.
    svn path=/plone.app.customerize/trunk/; revision=20227

--- plone/app/customerize/tests/layer.py
+++ plone/app/customerize/tests/layer.py
@@ -1,4 +1,13 @@
 from Products.PloneTestCase.layer import PloneSite
 
 class PloneCustomerize(PloneSite):
-    pass
+    # Derived layers *must* have setUp and tearDown
+    # methods, even when they are empty.
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        pass
