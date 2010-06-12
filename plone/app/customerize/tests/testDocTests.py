405691ec8e5944ba6271eb268c7dc3b448f11a3c
Hanno Schlichting <hanno@hannosch.eu>
2010-06-12 17:47:29 +0000

    Move tests into tests sub-package and avoid some magic.
    svn path=/plone.app.customerize/trunk/; revision=37173

--- plone/app/customerize/tests/testDocTests.py
+++ plone/app/customerize/tests/testDocTests.py
@@ -1,28 +1,25 @@
-# setup tests with all doctests found in docs/
-
-from plone.app.customerize import docs
-from plone.app.customerize.tests import layer
-from Testing.ZopeTestCase import FunctionalDocFileSuite
-from Products.PloneTestCase import PloneTestCase
-from Products.Five.testbrowser import Browser
+import doctest
 from unittest import TestSuite
-from os.path import join, split, abspath, dirname
-from os import walk
-from re import compile
-from sys import argv
 
+from Products.PloneTestCase import PloneTestCase
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+
+# BBB Zope 2.12
+try:
+    from Testing.testbrowser import Browser
+    Browser # pyflakes
+except ImportError:
+    from Products.Five.testbrowser import Browser
+
+from plone.app.customerize.tests import layer
 
 PloneTestCase.setupPloneSite()
 
-from zope.testing import doctest
-OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
-               doctest.ELLIPSIS |
-               doctest.NORMALIZE_WHITESPACE)
 
 class CustomerizeFunctionalTestCase(PloneTestCase.FunctionalTestCase):
 
     layer = layer.PloneCustomerize
-    
+
     def afterSetUp(self):
         """ set up the tests """
         pass
@@ -36,21 +33,14 @@ class CustomerizeFunctionalTestCase(PloneTestCase.FunctionalTestCase):
             browser.addHeader('Authorization', 'Basic %s:%s' % (user, pwd))
         return browser
 
-# we check argv to enable testing of explicitely named doctests
-if '-t' in argv:
-    pattern = compile('.*\.(txt|rst)$')
-else:
-    pattern = compile('^test.*\.(txt|rst)$')
 
 def test_suite():
     suite = TestSuite()
-    docs_dir = abspath(dirname(docs.__file__)) + '/'
-    for path, dirs, files in walk(docs_dir):
-        for name in files:
-            relative = join(path, name)[len(docs_dir):]
-            if not '.svn' in split(path) and pattern.search(name):
-                suite.addTest(FunctionalDocFileSuite(relative,
-                    optionflags=OPTIONFLAGS,
-                    package=docs.__name__,
-                    test_class=CustomerizeFunctionalTestCase))
+    OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+    for testfile in ('testBrowserLayers.txt', 'testCustomizeView.txt'):
+        suite.addTest(FunctionalDocFileSuite(testfile,
+                                optionflags=OPTIONFLAGS,
+                                package="plone.app.customerize.tests",
+                                test_class=CustomerizeFunctionalTestCase),
+                     )
     return suite
