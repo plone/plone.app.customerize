b8f7f0ffb999ccff0b21f77eb55f766b3fccb634
David Glick <davidglick@groundwire.org>
2011-02-16 07:24:12 +0000

    add missing security declaration
    svn path=/plone.app.customerize/branches/1.1/; revision=47611

--- plone/app/customerize/tool.py
+++ plone/app/customerize/tool.py
@@ -1,6 +1,9 @@
 from five.customerize.interfaces import IViewTemplateContainer
 from zope.interface import implements
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
 from OFS.Folder import Folder
+from Products.CMFCore.permissions import ManagePortal
 
 
 class ViewTemplateContainer(Folder):
@@ -12,12 +15,16 @@ class ViewTemplateContainer(Folder):
     title = 'Manages view customizations'
     meta_type = 'Plone View Customizations'
 
+    security = ClassSecurityInfo()
+
     manage_options = (
         dict(label='Registrations', action='registrations.html'),
         ) + Folder.manage_options[0:1] + Folder.manage_options[2:]
 
+    security.declareProtected(ManagePortal, 'addTemplate')
     def addTemplate(self, id, template):
         """ add the given ttw view template to the container """
         self._setObject(id, template)
         return getattr(self, id)
 
+InitializeClass(ViewTemplateContainer)
