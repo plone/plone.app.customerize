e0cf818aef97c2e62f3a21d5d166348f5547b601
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:22:09 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
     keywords='customerize plone views page templates zmi',
